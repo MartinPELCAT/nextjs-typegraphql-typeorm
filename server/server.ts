89787071eebc9d5ab950b9d771999c0bf436d2c5
Martin PELCAT <martinpelcat@hotmail.com>
2020-10-27 19:29:12 +0100

    add typeorm

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import { buildSchema } from "type-graphql";
 import next from "next";
 import express from "express";
 import UserResolver from "./src/resolvers/UserResolver";
+import { createConnection } from "typeorm";
 
 const nextApp = next({ dev: true });
 const handler = nextApp.getRequestHandler();
@@ -10,19 +11,29 @@ const handler = nextApp.getRequestHandler();
 export const server = async () => {
   return nextApp.prepare().then(async () => {
     const app = express();
+    /**
+     * typeorm setup
+     */
 
+    await createConnection();
+
+    /**
+     * Typegraphql setup
+     */
     const schema = await buildSchema({ resolvers: [UserResolver] });
+
     const apollo = new ApolloServer({
       schema,
       subscriptions: {
         path: "/api/sub",
       },
     });
+
     apollo.applyMiddleware({ path: "/api/gql", app });
 
-    app.all("*", (req, res) => handler(req, res)); // use page folder 
+    app.all("*", (req, res) => handler(req, res)); // use page folder
+    app.listen(3000);
 
-    await app.listen(3000);
     const { graphqlPath, subscriptionsPath } = apollo;
     console.info(`Server started at : ${graphqlPath}`);
     console.info(`Server started at : ${subscriptionsPath}`);

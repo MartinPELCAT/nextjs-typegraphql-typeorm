c8eaa7bb6d191f9931af07550cd2be796a95b824
Martin PELCAT <martinpelcat@hotmail.com>
2020-10-27 22:12:51 +0100

    fix injection

--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,12 @@ import { buildSchema } from "type-graphql";
 import next from "next";
 import express from "express";
 import UserResolver from "./src/resolvers/UserResolver";
-import { createConnection } from "typeorm";
-import { createServer } from "http2";
+import { createConnection, useContainer } from "typeorm";
+import Container from "typedi";
 
 const nextApp = next({ dev: true });
 const handler = nextApp.getRequestHandler();
-
+useContainer(Container);
 const PORT = 3000;
 
 export const server = async () => {
@@ -23,16 +23,21 @@ export const server = async () => {
     /**
      * Typegraphql setup
      */
-    const schema = await buildSchema({ resolvers: [UserResolver] });
+    const schema = await buildSchema({
+      resolvers: [UserResolver],
+      container: Container,
+    });
 
-    const apollo = new ApolloServer({ schema });
+    const apollo = new ApolloServer({
+      schema,
+      context: async ({ req, res }) => {
+        return { res, req };
+      },
+    });
     apollo.applyMiddleware({ path: "/api/gql", app });
     app.all("*", (req, res) => handler(req, res)); // use page folder
 
-    const httpServer = createServer(app);
-    apollo.installSubscriptionHandlers(httpServer);
-
-    httpServer.listen({ port: PORT }, () => {
+    app.listen({ port: PORT }, () => {
       console.log(
         `🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}`
       );

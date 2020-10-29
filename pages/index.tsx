39a587e7d953976fd29d591a0cc6361e21f159f6
Martin PELCAT <martinpelcat@hotmail.com>
2020-10-29 22:18:27 +0100

    add real uuid

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,9 @@ type Props = {
 };
 
 export default function Home({ user }: Props) {
-  const { data, loading } = useGetUserQuery({ variables: { userId: "123" } });
+  const { data, loading } = useGetUserQuery({
+    variables: { userId: "c403a673-9ed2-4def-9811-6ef455b3dd2e" },
+  });
 
   return (
     <div className="text-xl text-red-500">
@@ -26,7 +28,7 @@ export default function Home({ user }: Props) {
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const { data } = await client.query<GetUserQuery, GetUserQueryVariables>({
     query: GetUserDocument,
-    variables: { userId: "123" },
+    variables: { userId: "c403a673-9ed2-4def-9811-6ef455b3dd2e" },
   });
 
   return { props: { user: data.user } };
